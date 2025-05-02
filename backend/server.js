0689f48176b5caf3bcbbac07b3598885529c7522
Gautam <vishkarmaamrawati9@gmail.com>
2025-05-02 11:32:12 +0530

    port update

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const app=require('./app');
 const express=require('express')
 const path=require('path');
-let port=3000;
+let port=process.env.PORT||3000;
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'));
 app.use(express.static(path.join(__dirname,'public')));
