e3149a8d5cfa263495aaad7f38df475a6d4627da
Gautam <vishkarmaamrawati9@gmail.com>
2025-05-03 11:08:34 +0530

    APIKEY added

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,6 @@ const path = require('path');
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'));
 app.use(express.static(path.join(__dirname,'public')));
-let port=3000;
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
     app.post('/register',(req,res)=>{
--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,5 @@
 
-let apiKey ="2511f9f6117042b6aea772090c4ebf77";
+let apiKey =process.env.API_KEY||"2511f9f6117042b6aea772090c4ebf77";
 let url = `https://newsapi.org/v2/everything?q=Apple&from=2025-04-28&sortBy=popularity&apiKey=${apiKey}`;
 fetch(url)
    .then(response => {
