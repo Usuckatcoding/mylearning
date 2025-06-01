6c961221763d6e998e5902cbaf17804e000c75f5
Gautam <vishkarmaamrawati9@gmail.com>
2025-06-01 13:16:21 +0530

    Improved UI

--- frontend/app.js
+++ frontend/app.js
@@ -1,15 +1,20 @@
 const date = new Date();
-date.setDate(date.getDate() - 1); // Go back one day
+date.setDate(date.getDate() - 1);
 const formattedDate = date.toISOString().split('T')[0];
 let apiKey ="2511f9f6117042b6aea772090c4ebf77";
-let url = `https://newsapi.org/v2/everything?q=india&from=${formattedDate}&sortBy=popularity&apiKey=${apiKey}`;
-fetch(url)
+let url = `https://newsapi.org/v2/everything?q=us&from=${formattedDate}&sortBy=popularity&apiKey=${apiKey}`;
+setTimeout(()=>{
+   document.getElementById('lodincontainer').style.display='none';
+},1000)
+
+   fetch(url)
    .then(response => {
       console.log(response);
       return response.json();
    }
    )
    .then(information => {
+      document.getElementById('work0').style.display='flex';
       let a=0;
       let info=information.articles;
       let existingele=document.getElementById("work0");
@@ -27,12 +32,13 @@ fetch(url)
       console.log(container);
       const articleHTML = `
       <h2>${information.articles[a].title}</h2>
-      <p>${information.articles[a].description}</p>
-      <a href="${information.articles[a].url}" target="_blank">Read more</a>`;
+      <p >${information.articles[a].description}</p>
+      <a class='btn btn-primary btn-sm' href="${information.articles[a].url}" target="_blank">Read more</a>`;
       container.innerHTML = articleHTML;
       console.log(article);
       a++;
       }
       console.log(information);
    })
+
    
