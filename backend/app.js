5f922a130cf68466f1b420cf5708b065fd02dd72
Gautam <vishkarmaamrawati9@gmail.com>
2025-06-05 12:08:20 +0530

    database added

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-// const mysql = require('mysql2');
-// let connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     database: 'newsdata',
-//     password: '@Shizuka123'
-// })
+require('dotenv').config();
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,                // default SkySQL secure port
+  ssl: { rejectUnauthorized: true }  // very important for SkySQL SSL
+});
+connection.connect((err) => {
+  if (err) {
+    console.error('❌ SkySQL connection failed:', err.message);
+  } else {
+    console.log('✅ Connected to SkySQL successfully!');
+  }
+});
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -20,55 +30,56 @@ app.post('/login', (req, res) => {
 app.post('/signup', (req, res) => {
     res.render('signup');
 })
-// app.post('/home', (req, res) => {
-//     let signup = req.body.submitted;
-//     if (signup === 'Submit') {
-//         const username1 = req.body.username;
-//         const email = req.body.Email;
-//         const password = req.body.password;
-//         const checkpass = req.body.checkpassword;
-//         let userid = 1;
-//         let users = [[userid, username1, email, password]];
-//         connection.query('insert into users values ?', [users], (err, result) => {
-//             if (err) throw err
-//             console.log(result);
-//             if (!err) {
-//                 res.render('home');
-//             }
-//         })
-//     }
-//     else {
-//         const email = req.body.Email;
-//         const password = req.body.password;
-//         const sql=`select email,pasword from users where email='${email}'`
-//         connection.query(sql,(err,result)=>{
-//             if(err) throw err
-//             console.log(result);
-//             if(!result[0]){
-//                 // alert("There is no account");
-//                 console.log('There is no account');
-//                 res.redirect('/login');
-//             }
-//             else if(!result[0].email){
-//                 // alert('password or email is wrong');
-//                 console.log('user does not exist');
-//                 res.redirect('/login');
-//             }
-//             else if(result[0].pasword!= password){
-//                 console.log('password is wrong');
-//                 res.redirect('/login');
-//             }
-//             else if(result[0].email===email && result[0].pasword===password){
-//                 res.render('home');
-//             }
-//         })
-//     }
+app.post('/home', (req, res) => {
+    let signup = req.body.submitted;
+    if (signup === 'Submit') {
+        const username1 = req.body.username;
+        const email = req.body.Email;
+        const password = req.body.password;
+        const checkpass = req.body.checkpassword;
+        let userid = 1;
+        let users = [[userid, username1, email, password]];
+        connection.query('insert into users values ?', [users], (err, result) => {
+            if (err) throw err
+            console.log(result);
+            if (!err) {
+                res.render('home');
+            }
+        })
+    }
+    else {
+        const email = req.body.Email;
+        const password = req.body.password;
+        const sql=`select email,pasword from users where email='${email}'`
+        connection.query(sql,(err,result)=>{
+            if(err) throw err
+            console.log(result);
+            if(!result[0]){
+                // alert("There is no account");
+                console.log('There is no account');
+                res.redirect('/login');
+            }
+            else if(!result[0].email){
+                // alert('password or email is wrong');
+                console.log('user does not exist');
+                res.redirect('/login');
+            }
+            else if(result[0].pasword!= password){
+                console.log('password is wrong');
+                res.redirect('/login');
+            }
+            else if(result[0].email===email && result[0].pasword===password){
+                res.render('home');
+            }
+        })
+    }
 
-// })
+})
 app.get('/login', (req, res) => {
     res.render('login')
 })
 app.get('/signup', (req, res) => {
     res.render('signup')
 })
+// module.exports = pool.promise();
 module.exports = app;
--- frontend/app.js
+++ frontend/app.js
@@ -3,10 +3,6 @@ date.setDate(date.getDate() - 1);
 const formattedDate = date.toISOString().split('T')[0];
 let apiKey ="2511f9f6117042b6aea772090c4ebf77";
 let url = `https://newsapi.org/v2/everything?q=us&from=${formattedDate}&sortBy=popularity&apiKey=${apiKey}`;
-setTimeout(()=>{
-   document.getElementById('lodincontainer').style.display='none';
-},1000)
-
    fetch(url)
    .then(response => {
       console.log(response);
@@ -14,6 +10,7 @@ setTimeout(()=>{
    }
    )
    .then(information => {
+      document.getElementById('lodincontainer').style.display='none';
       document.getElementById('work0').style.display='flex';
       let a=0;
       let info=information.articles;
@@ -40,5 +37,9 @@ setTimeout(()=>{
       }
       console.log(information);
    })
-
+   .catch(error => {
+      console.error('Error fetching data:', error);
+      document.getElementById('work0').style.display='none';
+      document.getElementById('newsnotloaded').style.display='block';
+   });
    
